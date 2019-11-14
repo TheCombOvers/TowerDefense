3996653b6a5b36533e37463ae11b0b1c808e3a7f
cartwagon <54986646+cartwagon@users.noreply.github.com>
2019-11-13 21:04:11 -0500

    Add files via upload

---
+++ TowerDefenseGUI/Model Files/HighScores.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TowerDefenseGUI
+{
+    [Serializable()]
+    public class Highscores : ISerializable
+    {
+        public List<string> highscorelst = new List<string>();
+        private int score;
+        private string player;
+
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+            }
+        }
+
+        public string Player
+        {
+            get
+            {
+                return player;
+            }
+            set
+            {
+                player = value;
+            }
+        }
+
+        public Highscores()
+        {
+            Player = player;
+            Score = score;
+        }
+
+
+        public void AddHighscore(string player, int score)
+        {
+            highscorelst.Add(player + ": " + score + "\n");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Player", Player);
+            info.AddValue("Score", Score);
+        }
+
+        public Highscores(SerializationInfo info, StreamingContext context)
+        {
+            Player = (string)info.GetValue("Player", typeof(string));
+            Score = (int)info.GetValue("Score", typeof(int));
+        }
+    }
+}
---
+++ TowerDefenseGUI/Model Files/HighscoreTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace TowerDefenseGUI.Model_Files
+{
+    [TestFixture]
+    class HighscoreTest
+    {
+        [Test]
+        private void TestHighscoreAdd()
+        {
+            Highscores hs = new Highscores();
+            hs.AddHighscore("John", 34000);
+            hs.AddHighscore("Mark", 68000);
+            hs.AddHighscore("Lewis", 25000);
+            Assert.IsTrue(hs.highscorelst.Contains("John") && hs.highscorelst.Contains("34000") && hs.highscorelst.Contains("Mark") && hs.highscorelst.Contains("68000") && hs.highscorelst.Contains("Lewis") && hs.highscorelst.Contains("25000"));
+        }
+    }
+}
