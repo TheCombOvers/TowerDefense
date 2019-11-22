3b96fd8e93b4b32fc0ae7443c3d31d994248b187
mhane142 <mhane142@bju.edu>
2019-11-21 19:02:48 -0500

    Updated GameWindow.

--- TowerDefenseGUI/GameWindow.xaml.cs
+++ TowerDefenseGUI/GameWindow.xaml.cs
@@ -19,8 +19,8 @@ namespace TowerDefenseGUI.Model_Files
     /// </summary>
     public partial class GameWindow : Window
     {
-        int gamedifficulty;
         int money;
+        int waves;
         public GameWindow()
         {
             InitializeComponent();
@@ -29,8 +29,35 @@ namespace TowerDefenseGUI.Model_Files
         //difficulty is based on number: 0-easy, 1-medium, 2-hard
         public void SetMoneyAmount(int difficulty)
         {
-            this.gamedifficulty = difficulty;
-            
+            switch (difficulty)
+            {
+                case 0:
+                    money = 200;
+                    waves = 10;
+                    break;
+                case 1:
+                    money = 150;
+                    waves = 20;
+                    break;
+                case 2:
+                    money = 100;
+                    waves = 30;
+                    break;
+            }
+        }
+
+        public int SnapToGridX(int x)
+        {
+            int tempx = x % 50;
+            int newx = (tempx * 50) + 25;
+            return newx;
+        }
+
+        public int SnapToGridY(int y)
+        {
+            int tempy = y % 50;
+            int newy = (tempy * 50) + 25;
+            return newy;
         }
     }
 }
