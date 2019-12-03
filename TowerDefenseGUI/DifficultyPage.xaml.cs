c1beeda5ee00e0fec12308de6a48124e0e9a3f23
mhane142 <mhane142@students.bju.edu>
2019-12-03 15:39:25 -0500

    Added functionality to Main Menu button on Difficulty Page

--- TowerDefenseGUI/DifficultyPage.xaml.cs
+++ TowerDefenseGUI/DifficultyPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TowerDefenseGUI
 {
@@ -26,6 +14,12 @@ namespace TowerDefenseGUI
         {
             InitializeComponent();
             Cheat = cheat;
+        }
+        private void BtnMainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            Window.GetWindow(this).Close();
         }
 
         private void BtnSelectMapEasy_Click(object sender, RoutedEventArgs e)
