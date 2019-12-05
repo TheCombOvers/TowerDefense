df7a7ae8357df36655a150098e04cb812ad0c2b8
khans <khans685@students.bju.edu>
2019-12-05 18:05:47 -0500

    Code for High Scores

--- TowerDefenseGUI/Model Files/HighScores.cs
+++ TowerDefenseGUI/Model Files/HighScores.cs
@@ -6,62 +6,68 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace TowerDefenseGUI
 {
     [Serializable()]
-    public class Highscores : ISerializable
+    public class Highscores
     {
-        public List<string> highscorelst = new List<string>();
-        private int score;
-        private string player;
+        public static List<string> highscoreslist = new List<string>();
 
-        public int Score
+        public void GetNewScore(string highscore)
         {
-            get
+            highscoreslist.Add(highscore);
+        }
+
+        public static List<string> PassScores()
+        {
+            return highscoreslist;
+        }
+
+        public void Load(string filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
             {
-                return score;
+                string begin = reader.ReadLine();
+                if (begin == "Start")
+                {
+                    List<string> highscoreslist = new List<string>();
+                    string scorestring = "";
+                    while (scorestring != "END")
+                    {
+                        scorestring = reader.ReadLine();
+                        highscoreslist.Add(scorestring);
+                    }
+                }
             }
-            set
+        }
+
+        public void Save(string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
             {
-                score = value;
+                writer.WriteLine("Start");
+                for (int i = 0; i < highscoreslist.Count; i++)
+                {
+                    string highscoresstring = highscoreslist[i];
+                    writer.WriteLine(highscoresstring);
+                }
+                writer.WriteLine("END");
             }
+
         }
 
-        public string Player
-        {
-            get
-            {
-                return player;
-            }
-            set
-            {
-                player = value;
-            }
-        }
+        //public void GetObjectData(SerializationInfo info, StreamingContext context)
+        //{
+        //    info.AddValue("Player", Player);
+        //    info.AddValue("Score", Score);
+        //}
 
-        public Highscores()
-        {
-            Player = player;
-            Score = score;
-        }
-
-
-        public void AddHighscore(string player, int score)
-        {
-            highscorelst.Add(player + ": " + score + "\n");
-        }
-
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("Player", Player);
-            info.AddValue("Score", Score);
-        }
-
-        public Highscores(SerializationInfo info, StreamingContext context)
-        {
-            Player = (string)info.GetValue("Player", typeof(string));
-            Score = (int)info.GetValue("Score", typeof(int));
-        }
+        //public Highscores(SerializationInfo info, StreamingContext context)
+        //{
+        //    Player = (string)info.GetValue("Player", typeof(string));
+        //    Score = (int)info.GetValue("Score", typeof(int));
+        //}
     }
 }
