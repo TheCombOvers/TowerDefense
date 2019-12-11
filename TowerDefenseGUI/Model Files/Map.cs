c5e9df4934b965b0bc8a78e16358f858da17a8fb
ayerk481 <ary9302000@gmail.com>
2019-12-11 17:19:31 -0500

    added comments to map class

--- TowerDefenseGUI/Model Files/Map.cs
+++ TowerDefenseGUI/Model Files/Map.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// This file contains the Map class and Intersection Struct class
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,16 @@ using System.Windows.Media.Imaging;
 
 namespace TowerDefenseGUI
 {
+    // The map class holds the list of map paths, a reference to the
+    // chosen path, a enumerated type, and a value to specify the selected map
     public class Map
     {
-        public enum Direction { RIGHT, LEFT, UP, DOWN };
-        static List<Image> maps = new List<Image>();
-        static List<List<Intersection>> pathways = new List<List<Intersection>>();
-        public static Image map;
-        public static List<Intersection> coords;
-        public int mapID;
+        public enum Direction { RIGHT, LEFT, UP, DOWN }; // stores the direction for the path intersection
+        static List<List<Intersection>> pathways = new List<List<Intersection>>(); // list of map paths
+        public static List<Intersection> coords; // the selected path
+        public int mapID; // the selection of map
 
-        
+        // Hard codes the 3 map paths by identifying the intersections in the path.
         public Map(int id)
         {
             // map 1 coords ([200,0], [200,200], [350,200], [350,550], [150,550],[150,750],[600,750],[600,550],[900,550],[900,150],[700,150],[700,350],[550,350],[550,0])
@@ -49,7 +50,7 @@ namespace TowerDefenseGUI
             path.Add(new Intersection(475, 775, Direction.RIGHT));
             path.Add(new Intersection(475, 900, Direction.DOWN));
             pathways.Add(path);
-            // need to add path 3 coords
+            // map 3 coords ( [0,350], [1000,325] )
             path = new List<Intersection>();
             path.Add(new Intersection(0, 325, Direction.RIGHT));
             path.Add(new Intersection(1000, 325, Direction.RIGHT));
@@ -59,12 +60,14 @@ namespace TowerDefenseGUI
         }
     }
 
+    // A Struct that holds the x, y, and direction values of the map intersection
     public struct Intersection
     {
         public int x;
         public int y;
         public Map.Direction direction;
 
+        // assigns the x, y, and direction values
         public Intersection(int v1, int v2, Map.Direction dir)
         {
             x = v1;
